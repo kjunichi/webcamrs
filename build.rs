931c50b0cb6cea9545c752da2358f3d145799dfe
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-11-02 12:24:03 +0900

    Update opencv's dependencies

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 #[cfg(target_os="macos")]
 fn main() {
-    println!("cargo:rustc-link-search=native=/usr/local/opt/opencv3/lib");
+    println!("cargo:rustc-link-search=native=/usr/local/opt/opencv/lib");
 }
 
 #[cfg(not(target_os="macos"))]
